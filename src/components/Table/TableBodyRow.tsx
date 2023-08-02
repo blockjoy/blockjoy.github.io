e49f677eefe76148f0b846738b1e586ae9b04193
StjepanDzojic <stjepan@prototyp.digital>
2023-08-02 18:17:29 +0200

    Used new tablerow

--- src/components/Table/TableBodyRow.tsx
+++ src/components/Table/TableBodyRow.tsx
@@ -1,15 +1,44 @@
-import React, { FC } from 'react';
+import React, { FC, ReactNode, useState } from 'react';
+import Arrow from '@site/static/img/greater.svg';
+import style from './style.module.scss';
 
 interface Props {
-  items: string[];
+  items: (string | ReactNode)[];
+  rowCounter?: number;
+  children?: ReactNode;
 }
 
-export const TableBodyRow: FC<Props> = ({ items }) => {
+export const TableBodyRow: FC<Props> = ({
+  items,
+  children,
+  rowCounter = 0,
+}) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const isDropdownCell = (index: number) => children && index === 0;
+
   return (
-    <tr>
-      {items.map((item, i) => (
-        <td key={i}>{item}</td>
-      ))}
-    </tr>
+    <>
+      <tr>
+        {items?.map((item, i) => (
+          <td
+            className={isDropdownCell(i) ? style.cellClickable : undefined}
+            onClick={isDropdownCell(i) ? () => setIsOpen(!isOpen) : undefined}
+            style={
+              i == 0 ? { paddingLeft: `${12 + rowCounter * 20}px` } : undefined
+            }
+          >
+            {isDropdownCell(i) ? (
+              <span className={isOpen ? style['cell--active'] : style.cell}>
+                {<Arrow />}
+                {item}
+              </span>
+            ) : (
+              item
+            )}
+          </td>
+        ))}
+      </tr>
+      <>{isOpen && children}</>
+    </>
   );
 };
